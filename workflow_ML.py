aee9c382efd4f0f21efb4fdf9a58d660f3541cf5
Asif Iqbal Bhatti <AsifIqbal.Bhatti@UGent.be>
2022-12-16 16:27:41 +0100

    modified

--- workflow_ML.py
+++ workflow_ML.py
@@ -8,7 +8,6 @@
 #===================================================================
 
 '''
-	
 import subprocess, numpy, json, os
 from pymatgen.ext.matproj import MPRester
 from monty.serialization import loadfn
@@ -38,40 +37,48 @@ Dir          = "Dir"
 Subdir       = "Subdir"
 
 #============= INITIALIZATION
-data_cnt = 0; wf1 = 1
+data_cnt = 0
+wf1 = 1
 inp_Argo = 'DDDD'
 initial_path = os.getcwd()
 abs_path = os.path.abspath(os.path.join(initial_path, inp_Argo))
 argo_Dir = next(os.walk(abs_path))[1]
-print('2M_ARG:: ', str(len(argo_Dir)))
+print('2M_ARG:: ', len(argo_Dir))
 
 #============= EVOLUTION OF WORKFLOW
 dLDATA = ['A', 'B', 'C']
 
-outFile2.write("{},{},{},{},{},{},{}\n".format("WorkflowStep", "Dir", \
-	"Subdir", "IonicStep", "TotalEnergy/atom", "CorrTotalEnergy/atom", "ehull"))
+outFile2.write(
+	f"WorkflowStep,Dir,Subdir,IonicStep,TotalEnergy/atom,CorrTotalEnergy/atom,ehull\n"
+)
 
 with connect(outdbpathF) as conDB:
 	for p in tqdm(argo_Dir):
 		try:
-			nn = []; ee = []; ff = {}; ll = {}
-			oo = 1; asd = []; dife = []; dif4 = []
-			
+			nn = []
+			ee = []
+			ff = {}
+			ll = {}
+			oo = 1
+			asd = []
+			dife = []
+			dif4 = []
+
 			for dl in dLDATA:
 				arg_path  = os.path.join(inp_Argo, p, dl)
 				vasp_xml	= os.path.join(arg_path,'vasprun.xml')
 				read_xmlF = read(vasp_xml, index=':') # FINAL/ALL GEOMTERY (USE "-1" or ":")
-				
+
 				# WE OVERRIDE PYTHON BY INDEXING WITH 1!!!
 				for ionstep, at in enumerate(read_xmlF, start=1):	
-					nn.append("{}".format( ionstep ))
-					ee.append("{}".format( at.get_total_energy()/at.get_global_number_of_atoms() ))
-			
+					nn.append(f"{ionstep}")
+					ee.append(f"{at.get_total_energy() / at.get_global_number_of_atoms()}")
+
 			# REVERSE THE DATA FOR IDENTIFY THE TAG
 			nn = [int(i) for i in list(reversed(nn)) ]
 			ee = [float(i) for i in list(reversed(ee)) ]
 			hh = list(zip(nn,ee)); 
-			
+
 			# ANCHOR ON DIFFERENT DATA SETS!!! 
 			# ALL DATA NEEDS TO BE READ IN A WORKFLOW
 			for k, j in enumerate(hh, start=1):
@@ -84,7 +91,7 @@ with connect(outdbpathF) as conDB:
 					diff = temp-temp1	
 					ff[k] = diff, temp, temp1
 			#print(ff)
-			
+
 			# WE ARE INTERESTED IN THE LAST CALCULATED EHULL ONLY!!! 
 			# THE LAST ONE IS ENOUGH
 			read_EH = loadfn(os.path.join('workflow_data',dLDATA[2]+'.json'))
@@ -92,69 +99,72 @@ with connect(outdbpathF) as conDB:
 				for k1, v1 in read_EH[i].items():
 					if v1 == p:
 						k2 = x['Ehull']
+			key_list = list(ff.keys());
+			val_list = list(ff.values());
 			#print(p, k2)
-			
+
 			#=====================================================	
 			#                        MAIN
 			#=====================================================
-			
+
 			for dl in reversed(dLDATA):
 				arg_path  = os.path.join(inp_Argo, p, dl)
 				vasp_xml	= os.path.join(arg_path,'vasprun.xml')
 				read_xmlF = read(vasp_xml, index=':') # FINAL/ALL GEOMTERY (USE "-1" or ":")	
-					
+
 				# COUNT IONIC STEPS! & REVERSE IT
 				for ionstep, at in enumerate(read_xmlF, start = 1):	
 					ll[ionstep] = at.get_total_energy()/at.get_global_number_of_atoms()
-			
+
 				# FROM LAST TO FIRST ORDER!!!
 				rr = OrderedDict(reversed(list(ll.items())));
 				kk = list(rr.keys());
 				vv = list(rr.values());
-				key_list = list(ff.keys());
-				val_list = list(ff.values());
 				vvdiff = [i-vv[0] for i in vv];
-					
+
 				for u, at in enumerate(read_xmlF, start = 1):
 					if oo <=  key_list[0]:
 						conDB.write(at, IonStep=kk[u-1], EPerAtom=vv[u-1], Ehull=k2+vvdiff[u-1],
 						data={WS:wf1, Dir:p, Subdir:dl, IonStep:kk[u-1], EPerAtom:vv[u-1], CEPerAtom:vv[u-1], EHull:k2+vvdiff[u-1] })			
-						
+
 						dif1 = k2+vvdiff[u-1]
-						outFile2.write("{}, {}, {}, {}, {}, {}, {}\n". \
-						format(wf1, p, dl, kk[u-1], vv[u-1], vv[u-1], k2+vvdiff[u-1]  ))
-					
+						outFile2.write(
+							f"{wf1}, {p}, {dl}, {kk[u - 1]}, {vv[u - 1]}, {vv[u - 1]}, {k2 + vvdiff[u - 1]}\n"
+						)
+
 					if oo > key_list[0] and oo <= key_list[1]:
 						asd.append(vv[u-1]+val_list[0][0])
-						dif2 = [i-asd[0] for i in asd] 
+						dif2 = [i-asd[0] for i in asd]
 						dif3 = dif1+dif2[u-1]
-						
+
 						conDB.write(at, IonStep=kk[u-1], EPerAtom=vv[u-1], Ehull=dif1+dif2[u-1] , 
 						data={WS:wf1, Dir:p, Subdir:dl, IonStep:kk[u-1], EPerAtom:vv[u-1], CEPerAtom:vv[u-1]+val_list[0][0], EHull:dif1+dif2[u-1] })
-						
-						outFile2.write("{}, {}, {}, {}, {}, {}, {}\n". \
-						format(wf1, p, dl, kk[u-1], vv[u-1], vv[u-1]+val_list[0][0], dif1+dif2[u-1]    ))
-				
+
+						outFile2.write(
+							f"{wf1}, {p}, {dl}, {kk[u - 1]}, {vv[u - 1]}, {vv[u - 1] + val_list[0][0]}, {dif1 + dif2[u - 1]}\n"
+						)
+
 					if oo > key_list[1]:
 						dife.append( vv[u-1]-asd[-1] )
 						dif4 = [i-dife[0] for i in dife]
-						
+
 				for u, at in enumerate(read_xmlF, start = 1):
 					if oo > key_list[1]:	
-					
+
 						conDB.write(at, IonStep=kk[u-1], EPerAtom=vv[u-1], Ehull=dif3+dif4[u-1],
 						data={WS:wf1, Dir:p, Subdir:dl, IonStep:kk[u-1], EPerAtom:vv[u-1], CEPerAtom:vv[u-1]-dife[0], EHull:dif3+dif4[u-1]  })
-						
-						outFile2.write("{}, {}, {}, {}, {}, {}, {}\n". \
-						format(wf1, p, dl, kk[u-1], vv[u-1], vv[u-1]-dife[0], dif3+dif4[u-1]     ))		
-						
+
+						outFile2.write(
+							f"{wf1}, {p}, {dl}, {kk[u - 1]}, {vv[u - 1]}, {vv[u - 1] - dife[0]}, {dif3 + dif4[u - 1]}\n"
+						)		
+
 					data_cnt +=1
 					oo +=1
 			wf1 +=1
 		except:
 			pass
-print("Total_Entries_in_db:: {}".format(data_cnt))
-print("Total_workflow_in_db:: {}".format(wf1-1))
+print(f"Total_Entries_in_db:: {data_cnt}")
+print(f"Total_workflow_in_db:: {wf1 - 1}")
 
 df = pd.read_csv('EHCorr.csv')
 print (df[["IonicStep", "TotalEnergy/atom", "ehull"]])
